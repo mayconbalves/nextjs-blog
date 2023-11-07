821b9688c91d20d75117c32cc4d5eb86617400fc
mayconbalves <mayconbalves1@gmail.com>
2023-11-06 23:14:48 -0300

    removed server folder

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-export const fetchData = (value) => {
-  axios.get(`http://api.diretrixconsultoria.com.br/Consultas/Pessoa/${value}`, {
-      headers: {
-        'Authorization': 'Basic cmFmYWVsOjEwMTAxMA=='
-      },
-    }).then(resp => resp.data)
-}
